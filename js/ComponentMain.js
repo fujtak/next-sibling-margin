cfbf24b0f6812b63d1c0442db978d6cd9aac9b3d
fujtak <fuj.eng23@gmail.com>
2026-03-15 16:56:26 +0900

    add: クエリで初期値を設定できるように

--- js/ComponentMain.js
+++ js/ComponentMain.js
@@ -6,12 +6,26 @@ class ComponentMainSelector {
     this.value = this.element.value
     Object.freeze(this)
   }
+  set(value) {
+    if(value) this.element.value = value
+    return new ComponentMainSelector()
+  }
+  static forLoad() {
+    const stale = new ComponentMainSelector()
+    const params = new URL(document.location).searchParams
+    const value = params.get('main')
+    return stale.set(value)
+  }
 }
 
 export class ComponentMain {
-  constructor() {
-    this.selector = new ComponentMainSelector()
+  constructor(selector) {
+    this.selector = selector ?? new ComponentMainSelector()
     this.isVarid = this.selector.isVarid
     Object.freeze(this)
+  }
+  static forLoad() {
+    const selector = ComponentMainSelector.forLoad()
+    return new ComponentMain({ selector })
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import { generator } from "./Generator.js";
 import { componentNextAdd } from "./ComponentNextAdd.js";
 
+import { ComponentMain } from "./ComponentMain.js";
+
 generator.initialize()
 componentNextAdd.initialize()
+
+ComponentMain.forLoad()
