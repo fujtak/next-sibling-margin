affe25ac16f6ee382b565850a0c0c419c9eaa5aa
fujtak <fuj.eng23@gmail.com>
2026-03-18 00:40:12 +0900

    change: DOM更新を一時停止するホバー範囲を狭める

--- js/GeneratorCodeDisplay.js
+++ js/GeneratorCodeDisplay.js
@@ -1,11 +1,10 @@
 class GeneratorCodeDisplay {
   constructor() {
-    this.root = document.querySelector('[data-display="root"]')
     this.element = document.querySelector('[data-code-display="wrapper"]')
     Object.freeze(this)
   }
   get #isHover() {
-    return this.root.matches(':hover')
+    return this.element.matches(':hover')
   }
   update(code) {
     if(this.#isHover) return
--- js/GeneratorShareDisplay.js
+++ js/GeneratorShareDisplay.js
@@ -1,12 +1,11 @@
 
 class GeneratorShareDisplay {
   constructor() {
-    this.root = document.querySelector('[data-display="root"]')
     this.element = document.querySelector('[data-share-display="wrapper"]')
     Object.freeze(this)
   }
   get #isHover() {
-    return this.root.matches(':hover')
+    return this.element.matches(':hover')
   }
   update(url) {
     if(this.#isHover) return
